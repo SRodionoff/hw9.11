e0a0b5f05bbf41fc0615262e4fe968885fd9a6f6
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-11-09 07:15:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+string[] arr = { "hello", "2", "World", "-2", "elf", "else", "if", "other", "bish bash bosh" };
+Console.WriteLine($"Изначальный массив: [ {string.Join(", ", arr)} ]");
+Console.Write("Введите максимальное количество символов в строке: ");
+int symbols = Convert.ToInt32(Console.ReadLine());
+List<string> newArr = new List<string>();
+for (int i = 0; i < arr.Length; i++)
+{
+   if (arr[i].Length <= symbols)
+   {
+      newArr.Add(arr[i]);
+   }
+}
+Console.Write("Итоговый массив: " + "[\"" + string.Join("\", \"", newArr) + "\"]");
